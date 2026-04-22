a061b19ebc9416e91e959bd931d702e83633cf3e
Daniel Stern <daniel@herald.to>
2026-04-22 07:07:47 -0400

    .

---
+++ etc.js
@@ -0,0 +1,19 @@
+import yaml from "js-yaml"
+import { readdirSync, readFileSync } from "fs"
+
+export async function get_user_by_username({
+    username : input_username
+}) {
+    let raw = readFileSync("data-unsafe.yaml")
+    let data = yaml.load(raw)
+
+    let { 
+        users
+    } = data
+
+    let user = users.find(
+        user => user.username === input_username
+    ) ?? null
+
+    return user
+}
